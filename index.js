1b8b0b0af78cc054535bf8c9a0dd8e3e95bd57b2
Recep Demir <demir.rp@gmail.com>
2025-05-28 12:37:57 +0200

    feat: establish MongoDB connection with mongoose

--- index.js
+++ index.js
@@ -3,8 +3,20 @@
 const express = require("express");
 const app = express();
 
+
+
 require('dotenv').config();
 const HOST = process.env?.HOST || '127.0.0.1'
 const PORT = process.env?.PORT || 8000
 
 app.set("query parser", "extended");
+
+const { dbConnection } = require('./src/configs/dbConnection');
+dbConnection();
+
+
+
+
+
+
+app.listen(PORT ,HOST, ()=> console.log(`http://${HOST}:${PORT}`))
