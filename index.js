d9f2b78a5588f5497debb6592e25c44e9075146c
Recep Demir <demir.rp@gmail.com>
2025-05-28 12:56:34 +0200

    feat: remove extended query parser setting from express app

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ require('dotenv').config();
 const HOST = process.env?.HOST || '127.0.0.1'
 const PORT = process.env?.PORT || 8000
 
-app.set("query parser", "extended");
 
 const { dbConnection } = require('./src/configs/dbConnection');
 dbConnection();
