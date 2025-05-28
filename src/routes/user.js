e93666283f8af2ccaf29161b3f957cafef3ebf92
Recep Demir <demir.rp@gmail.com>
2025-05-28 21:53:22 +0200

    refactor: streamline user route by consolidating imports and improving structure

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,11 @@
 
 const router = require("express").Router();
 
-const permissions = require("../middlewares/permissions");
-const user = require("../controllers/user");
+const { list, create, read, update, deletee } = require('../controllers/user');
+const { isAdmin,islogin } = require('../middlewares/permissions');
+
+router.route('/').get(isAdmin,list).post(islogin,create);
+router.route('/:id').get(read).put(islogin,update).patch(islogin,update).delete(isAdmin,deletee)
+
+module.exports = router
 
