c5213f95b8ba969573ab87b7ce710da8df426387
Recep Demir <demir.rp@gmail.com>
2025-05-29 14:41:11 +0200

    feat: add category routes for CRUD operations with permission checks

---
+++ src/routes/category.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const router = require("express").Router();
+
+const { list, create, read, update, deletee } = require('../controllers/category');
+const { isAdmin,isLogin } = require('../middlewares/permissions');
+
+
+router.route('/').get(isLogin, list).post(isLogin,create);
+router.route('/:id').get(read).put(isAdmin,update).patch(isAdmin,update).delete(isAdmin,deletee)
+
+module.exports = router
+
