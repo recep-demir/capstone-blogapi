282380fb92bb884ade4f4569abe3b58cbbf8319e
Recep Demir <demir.rp@gmail.com>
2025-05-29 18:43:18 +0200

    feat: implement CRUD operations for Comment controller

---
+++ src/controllers/comment.js
@@ -0,0 +1,105 @@
+"use strict"
+
+const Comment = require('../models/comment');
+const CustomError = require('../helpers/customError');
+
+module.exports = {
+
+    list: async (req, res) => {
+        /*
+            #swagger.tags = ["Comments"]
+            #swagger.summary = "List Comments"
+            #swagger.description = `
+                You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
+                    <li>URL/?<b>limit=10&page=1</b></li>
+                </ul>
+            `
+        */
+
+        const result = await res.getModelList(Comment,{},['userId','blogId']);
+
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(Comment),
+            result
+        });
+    },
+
+    create: async (req, res) => {
+        /*
+            #swagger.tags = ["Comments"]
+            #swagger.summary = "Create Comment"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                   $ref: "#/definitions/Comment"
+                }
+            }
+        */
+
+        const result = await Comment.create(req.body);
+
+        res.status(201).send({
+            error: false,
+            result
+        });
+    },
+
+    read: async (req, res) => {
+        /*
+            #swagger.tags = ["Comments"]
+            #swagger.summary = "Get Single Comment"
+        */
+
+        const result = await Comment.findById(req.params.id).populate(['userId','blogId']);
+
+        res.status(200).send({
+            error: false,
+            result
+        });
+    },
+
+    update: async (req, res) => {
+        /*
+            #swagger.tags = ["Comments"]
+            #swagger.summary = "Update Comment"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                   $ref: "#/definitions/Comment"
+                }
+            }
+        */
+
+        const result = await Comment.findByIdAndUpdate(req.params.id, req.body, { runValidators: true, new: true });
+
+        if (!result) throw new CustomError("Update failed, comment is not found or already updated", 404);
+
+        res.status(202).send({
+            error: false,
+            result
+        });
+    },
+
+    deletee: async (req, res) => {
+        /*
+            #swagger.tags = ["Comments"]
+            #swagger.summary = "Delete Comment"
+        */
+
+        const result = await Comment.findByIdAndDelete(req.params.id)
+
+        if (!result) throw new CustomError("Delete failed, data is not found or already deleted", 404);
+
+        res.status(200).send({
+            error: false,
+            result
+        });
+    },
+}
