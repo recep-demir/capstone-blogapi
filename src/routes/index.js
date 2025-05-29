7bdd3b737214a38aa6ac66db0cc6bdd68e97aa8c
Recep Demir <demir.rp@gmail.com>
2025-05-29 18:47:21 +0200

    feat: add comments route to the main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,7 @@ const router = require('express').Router()
 router.use('/blogs', require('./blog'))
 router.use("/user", require("./user"));
 router.use("/categories", require("./category"));
-
-
+router.use("/comments", require("./comment"));
 
 
 router.use('/documents', require('./document'));
