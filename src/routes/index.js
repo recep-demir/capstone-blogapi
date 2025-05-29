591babef726be1b990104e7fc0817cc739bbabf3
Recep Demir <demir.rp@gmail.com>
2025-05-29 18:53:01 +0200

    fix: reorder route definitions for consistency

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,11 @@
 
 const router = require('express').Router()
 
-router.use('/blogs', require('./blog'))
+router.use("/auth", require("./auth"));
+router.use("/token", require("./token"));
 router.use("/user", require("./user"));
+router.use('/blogs', require('./blog'))
+
 router.use("/categories", require("./category"));
 router.use("/comments", require("./comment"));
 
