1dfc7109d28dcae1cffc1fd1de56c5c0cac6992a
Recep Demir <demir.rp@gmail.com>
2025-05-29 15:41:10 +0200

    fix: format code for consistency in user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,7 +6,7 @@ const { list, create, read, update, deletee } = require('../controllers/user');
 const { isAdmin,isLogin } = require('../middlewares/permissions');
 
 
-router.route('/').get(isAdmin, list).post(isLogin,create);
+router.route('/').get(isAdmin, list).post(isLogin, create);
 router.route('/:id').get(read).put(isLogin,update).patch(isLogin,update).delete(isAdmin,deletee)
 
 module.exports = router
