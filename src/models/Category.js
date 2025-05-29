cd73726b473889e3f99818f592d7fc283bc56671
Recep Demir <demir.rp@gmail.com>
2025-05-29 14:23:11 +0200

    feat: create category model with schema and export

---
+++ src/models/Category.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const { mongoose } = require('../configs/dbConnection')
+
+const categorySchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    }
+
+}, {
+    collection: 'categories',
+    timestamps: true
+});
+
+module.exports = mongoose.model("Category", categorySchema);
