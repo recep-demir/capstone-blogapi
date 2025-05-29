b6a17fff12fd1321111530b6203d15d43338ea52
Recep Demir <demir.rp@gmail.com>
2025-05-29 16:20:38 +0200

    feat: create Comment model with schema definition

---
+++ src/models/Comment.js
@@ -0,0 +1,27 @@
+"use strict"
+
+const {mongoose}= require("../configs/dbConnection")
+
+const CommentSchema = new mongoose.schema({
+    userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+    },
+
+    blogId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Blog',
+    required: true
+    },
+    comment:{
+        type:String,
+        required: true,
+
+    }
+
+
+},{collection: 'categories',
+    timestamps: true})
+
+    module.exports = mongoose.model("Comment",CommentSchema)
