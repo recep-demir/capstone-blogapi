fb9be5e7366debbd20452476b70f13e6b4fd34ef
Recep Demir <demir.rp@gmail.com>
2025-05-30 22:15:33 +0200

    fix: remove unused isAdmin middleware from comment routes

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -3,11 +3,10 @@
 const router = require("express").Router();
 
 const { list, create, read, update, deletee } = require('../controllers/comment');
-const { isAdmin,isLogin } = require('../middlewares/permissions');
+const {isLogin } = require('../middlewares/permissions');
 
 
 router.route('/').get(list).post(isLogin,create);
 router.route('/:id').get(read).put(isLogin,update).patch(isLogin,update).delete(isLogin,deletee)
 
 module.exports = router
-
