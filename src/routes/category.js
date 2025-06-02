f1c89eee75e3c710f32ca324bfd65f79e6949aa0
Recep Demir <demir.rp@gmail.com>
2025-06-02 21:33:24 +0200

    fix: add admin permission check for category creation route

--- src/routes/category.js
+++ src/routes/category.js
@@ -6,7 +6,7 @@ const { list, create, read, update, deletee } = require('../controllers/category
 const { isAdmin,isLogin } = require('../middlewares/permissions');
 
 
-router.route('/').get(list).post(create);
+router.route('/').get(list).post(isAdmin,create);
 router.route('/:id').get(read).put(isAdmin,update).patch(isAdmin,update).delete(isAdmin,deletee)
 
 module.exports = router
